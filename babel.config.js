893afd7c26104f161df07f1b0d56d9ab7c6567f0
Keerthieaaswar <keerthieaaswar@gmail.com>
2021-11-17 16:37:13 +0530

    Added alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.tsx', '.json'],
+        alias: {
+          'assets': './src/assets',
+          'components': './src/components',
+          'config': './src/config',
+          'navigation': './src/navigation',
+          'utilities': './src/utilities',
+          'views': './src/views',
+        },
+      },
+    ],
+  ],
 };
