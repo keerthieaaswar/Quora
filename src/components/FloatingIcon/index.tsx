fc38b6b519ad4add0449cb15a874113a8a1caa1c
Keerthieaaswar <keerthieaaswar@gmail.com>
2021-11-19 10:48:26 +0530

    added touchanbleOpacity

--- src/components/FloatingIcon/index.tsx
+++ src/components/FloatingIcon/index.tsx
@@ -1,13 +1,14 @@
 import PlusSvg from "assets/svgs/PlusSvg";
 import React from "react";
-import { View } from "react-native";
+import { TouchableOpacity } from "react-native";
 import styles from "./styles";
 
 const FloatingIcon: React.FC = () => {
   return (
-    <View style={styles.container}>
+    <TouchableOpacity style={styles.container}>
       <PlusSvg size={1.2} />
-    </View>
+    </TouchableOpacity>
+
   );
 };
 
